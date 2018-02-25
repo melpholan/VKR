3394a8318dad99460a71603c699acef9b0144bf0
melpholan <melpholan@yandex.ru>
2018-02-25 23:51:09 +0300

    First commit

---
+++ src/main/java/ru/melpholan/services/chiefServices/PersonalService.java
@@ -0,0 +1,9 @@
+package ru.melpholan.services.chiefServices;
+
+import ru.melpholan.entity.Personal;
+
+public interface PersonalService {
+
+    void createPersonal(Personal personal);
+    void updatePersonal();
+}
